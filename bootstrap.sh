1346adbb812f01d339198617bd5e7a2c824682a5
Jo Geraerts <jo.geraerts@softwareag.com>
2023-05-26 01:10:47 +0200

    update new roles + package reconfigurationn

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 xcode-select --install
-sudo easy_install pip
-pip install --user ansible
-~/Library/Python/2.7/bin/ansible-galaxy install -r requirements.yml
+pip3 install --user ansible
+~/Library/Python/3.9/bin/ansible-galaxy install -r requirements.yml
+~/Library/Python/3.9/bin/ansible-playbook main.yml -i inventory --ask-become-pass
